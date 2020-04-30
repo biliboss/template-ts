36c5ee7e69d02ad9eb5df8675d80d0163521a49b
Gabriel Fonseca <gabriel@devmagic.com.br>
2020-04-30 20:03:39 -0300

    Template of a simple Typescript + Jest (Project)

---
+++ sum.ts
@@ -0,0 +1,4 @@
+export function sum(a: number, b: number) {
+  console.log(a + b);
+  return a + b;
+}
---
+++ test.spec.ts
@@ -0,0 +1,5 @@
+import {sum} from "./sum";
+
+test('Sample', () => {
+  expect(sum(2, 1)).toEqual(2);
+});
